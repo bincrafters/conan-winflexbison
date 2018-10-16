138d94bc0bb2985b5a3d2324aa4fe8988cba8039
danimtb <danimanzaneque@gmail.com>
2018-10-16 17:51:43 +0200

    Use new recipe conventions

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
 import os
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
 
 
 class WinflexbisonConan(ConanFile):
@@ -14,36 +15,36 @@ class WinflexbisonConan(ConanFile):
     author = "Bincrafters <bincrafters@gmail.com>"
     license = "Several licenses"
     exports = ["LICENSE.md"]
-    exports_sources = ["CMakeLists.txt", "*.patch"]
+    exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
+    default_options = {"shared": False}
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
     def config_options(self):
         if self.settings.os != "Windows":
-            raise Exception("winflexbison is only supported on Windows.")
+            raise ConanInvalidConfiguration("winflexbison is only supported on Windows.")
 
     def source(self):
         tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version), sha256="39fe57de6a52dc83c8a9ece90b8484d8d2b764e24e22e30ba5dc018328019a4d")
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
-    def configure_cmake(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.configure(build_folder=self.build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
-
-        actual_build_path = "{}/bin/{}".format(self.source_subfolder, self.settings.build_type)
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        actual_build_path = "{}/bin/{}".format(self._source_subfolder, self.settings.build_type)
         self.copy(pattern="*.exe", dst="bin", src=actual_build_path, keep_path=False)
         self.copy(pattern="data/*", dst="bin", src=actual_build_path, keep_path=True)
         self.copy(pattern="*.h", dst="include", src=actual_build_path, keep_path=False)
