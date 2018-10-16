c93b47e0b490fb4a20d6144c9825998d32b83965
danimtb <danimanzaneque@gmail.com>
2018-10-16 18:12:50 +0200

    generate GPLv3 license from header file

--- conanfile.py
+++ conanfile.py
@@ -13,7 +13,7 @@ class WinflexbisonConan(ConanFile):
     url = "https://github.com/bincrafters/conan-winflexbison"
     homepage = "https://github.com/lexxmark/winflexbison"
     author = "Bincrafters <bincrafters@gmail.com>"
-    license = "Several licenses"
+    license = "GPLv3"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
@@ -21,7 +21,6 @@ class WinflexbisonConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = {"shared": False}
     _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
 
     def config_options(self):
         if self.settings.os != "Windows":
@@ -31,6 +30,13 @@ class WinflexbisonConan(ConanFile):
         tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version), sha256="39fe57de6a52dc83c8a9ece90b8484d8d2b764e24e22e30ba5dc018328019a4d")
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
+        # Generate license from header of a source file
+        with open("%s/%s/bison/data/glr.cc" % (self.source_folder, self._source_subfolder)) as f:
+            content_lines = f.readlines()
+        license_content = []
+        for i in range(2, 16):
+            license_content.append(content_lines[i][2:-1])
+        tools.save("%s/%s/LICENSE" % (self.source_folder, self._source_subfolder), "\n".join(license_content))
 
     def _configure_cmake(self):
         cmake = CMake(self)
