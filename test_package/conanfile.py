91a2fcad8993cb58c3af375837486dd93805502e
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-03-12 18:46:42 +0100

    Added bison and flex tests

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake
-import os
+from conans import ConanFile, CMake, tools, RunEnvironment
+
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
         self.run("win_flex --version", run_environment=True)
         self.run("win_bison --version", run_environment=True)
         self.run("flex --version", run_environment=True)
         self.run("bison --version", run_environment=True)
+
+        if not tools.cross_building(self.settings):
+            with tools.environment_append(RunEnvironment(self).vars):
+                cmake = CMake(self)
+                cmake.test()
